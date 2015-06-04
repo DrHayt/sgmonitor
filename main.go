59f21cb9d2766065f5a494bd27eed955d7f34940
Rick Moran <drhayt@gmail.com>
2015-06-04 09:13:39 -0400

    Works sort of

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"os"
 
 	"github.com/c9s/goprocinfo/linux"
 	"github.com/mgutz/logxi/v1"
@@ -12,22 +12,29 @@ var logger log.Logger
 
 func main() {
 
+	fmt.Println("SGMonitor starting up")
 	logger = log.New("sgmonitor")
 	logger.Trace("attempting to read /proc/cpuinfo")
-	cpuinfo, err := linux.ReadCPUInfo("proc/cpuinfo")
+	var realpath = "/proc/cpuinfo"
+	var fakepath = "proc/cpuinfo"
+
+	var procpath string
+
+	//  Check to see if /proc/cpuinfo exists, if it does
+	if _, err := os.Stat(realpath); os.IsNotExist(err) {
+		procpath = fakepath
+	} else {
+		procpath = realpath
+	}
+	cpuinfo, err := linux.ReadCPUInfo(procpath)
 	if err != nil {
 		logger.Fatal("Unable to get cpuinfo", "ReadCPUInfo")
 	}
 	logger.Trace("read cpuinfo")
 
-	logger.Info("We have this many cpus", "ReadCPUInfo", cpuinfo.Processors)
+	logger.Info("We have this many cpus", "ReadCPUInfo", len(cpuinfo.Processors))
+	logger.Info("We have this many Cores", "ReadCPUInfo", cpuinfo.NumCore())
 
-	// logger.Debug("This is a debug log entry")
-	// logger.Info("This is a info log entry")
-	// logger.Warn("This is a warn log entry")
-	// logger.Error("This is a error log entry")
-	// logger.Fatal("This is a fatal log entry")
-	// logger.log("This is a generic log entry")
+	// fmt.Printf("%+v\n", cpuinfo)
 
-	logger.Trace("I called stuff")
 }
